3dc78b9bae771a3ba3f1853a52a010a207353b40
Bilal Ahmad <pro123908@gmail.com>
2020-10-11 20:39:56 +0500

    regular commit

--- src/containers/Update/Update.js
+++ src/containers/Update/Update.js
@@ -37,7 +37,7 @@ const Login = () => {
                 {key} : {lsLinks[key]}
               </div>
             ))
-          : "Loading..."}
+          : "Loading.."}
       </div>
     </div>
   );
